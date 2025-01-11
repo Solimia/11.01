073f892b1a02fdad78fb493d7e7b0e6538689f6a
solomiya <solomiya@DESKTOP-GU8CTIH>
2025-01-11 16:15:19 +0200

    Add project files.

---
+++ 11.01/Array.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._01
+{
+
+    public class ArrayOperations : ICalc
+    {
+        private int[] array;
+
+        public ArrayOperations(int[] inputArray)
+        {
+            array = inputArray;
+        }
+
+
+        public int Less(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item < valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        public int Greater(int valueToCompare)
+        {
+            int count = 0;
+            foreach (int item in array)
+            {
+                if (item > valueToCompare)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ 11.01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using _11._01;
+
+class Program
+{
+    static void Main()
+    {
+
+        int[] arr = { 1, 5, 8, 3, 10, 12, 7 };
+
+        ArrayOperations operations = new ArrayOperations(arr);
+
+        int lessThanValue = 7;
+        int lessCount = operations.Less(lessThanValue);
+        Console.WriteLine($"Кількість елементів, менших за {lessThanValue}: {lessCount}");
+
+        int greaterThanValue = 7;
+        int greaterCount = operations.Greater(greaterThanValue);
+        Console.WriteLine($"Кількість елементів, більших за {greaterThanValue}: {greaterCount}");
+    }
+}

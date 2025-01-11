f5a55bf180d2c15c9b0acec75cc8771967e82597
solomiya <solomiya@DESKTOP-GU8CTIH>
2025-01-11 16:20:36 +0200

    cccc

---
+++ 2ex/Array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2ex
+{
+    public class ArrayOperations : IOutput2
+    {
+        private int[] array;
+
+        public ArrayOperations(int[] inputArray)
+        {
+            array = inputArray;
+        }
+
+        public void ShowEven()
+        {
+            Console.WriteLine("Парні значення:");
+            foreach (int item in array)
+            {
+                if (item % 2 == 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        public void ShowOdd()
+        {
+            Console.WriteLine("Непарні значення:");
+            foreach (int item in array)
+            {
+                if (item % 2 != 0)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ 2ex/Program.cs
@@ -0,0 +1,14 @@
+﻿using _2ex;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 5, 8, 3, 10, 12, 7 };
+
+        ArrayOperations operations = new ArrayOperations(arr);
+
+        operations.ShowEven();
+        operations.ShowOdd();
+    }
+}

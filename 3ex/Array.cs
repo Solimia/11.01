6be6c09ec819e1391029392f20f1f00d497cca82
solomiya <solomiya@DESKTOP-GU8CTIH>
2025-01-11 16:28:07 +0200

    tttttt

---
+++ 3ex/Array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ex
+{
+
+    public class ArrayOperations : ICalc2
+    {
+        private int[] array;
+
+
+        public ArrayOperations(int[] inputArray)
+        {
+            array = inputArray;
+        }
+
+        public int CountDistinct()
+        {
+            return array.Distinct().Count();
+        }
+
+        public int EqualToValue(int valueToCompare)
+        {
+            return array.Count(x => x == valueToCompare);
+        }
+    }
+}
---
+++ 3ex/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3ex
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 5, 8, 3, 10, 12, 7, 5, 8 };
+
+            ArrayOperations operations = new ArrayOperations(arr);
+
+            int distinctCount = operations.CountDistinct();
+            Console.WriteLine($"Кількість унікальних значень: {distinctCount}");
+
+            int valueToCompare = 8;
+            int equalCount = operations.EqualToValue(valueToCompare);
+            Console.WriteLine($"Кількість значень, рівних {valueToCompare}: {equalCount}");
+        }
+    }
+
+}
